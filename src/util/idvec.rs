e144fb235770099b16c841c6aaa5c396453d93c3
Ian Shehadeh <ian@shehadeh.net>
2024-05-03 17:23:17 -0400

    new syntax tree container

--- src/util/idvec.rs
+++ src/util/idvec.rs
@@ -1,93 +1,141 @@
-use std::{collections::BTreeMap, hash::Hash, marker::PhantomData, sync::atomic::AtomicUsize};
+use std::{hash::Hash, marker::PhantomData, sync::atomic::AtomicUsize};
 
-#[derive(Debug)]
-pub struct Id<Marker> {
+#[derive(Debug, Copy, Eq, PartialEq, Hash, Ord, PartialOrd)]
+pub struct Id<MarkerT> {
     element_id: usize,
-    container_id: usize,
-    marker: PhantomData<Marker>,
+    token: IdVecToken<MarkerT>,
 }
 
 impl<Marker> Clone for Id<Marker> {
+    fn clone(&self) -> Self {
+        Id {
+            element_id: self.inner(),
+            token: self.token,
+        }
+    }
+}
+
+impl<MarkerT> Id<MarkerT> {
+    pub fn inner(&self) -> usize {
+        self.element_id
+    }
+}
+
+#[derive(Debug)]
+pub struct IdVecToken<MarkerT> {
+    container_id: usize,
+    marker: PhantomData<MarkerT>,
+}
+
+impl<MarkerT> IdVecToken<MarkerT> {
+    pub fn inner(&self) -> usize {
+        self.container_id
+    }
+
+    pub fn owns(&self, id: Id<MarkerT>) -> bool {
+        *self == id.token
+    }
+
+    pub fn new() -> IdVecToken<MarkerT> {
+        static VEC_ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
+        let container_id = VEC_ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+        IdVecToken {
+            container_id,
+            marker: PhantomData::default(),
+        }
+    }
+}
+
+impl<Marker> Clone for IdVecToken<Marker> {
     fn clone(&self) -> Self {
         *self
     }
 }
 
-impl<Marker> Hash for Id<Marker> {
+impl<Marker> Hash for IdVecToken<Marker> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.element_id.hash(state);
         self.container_id.hash(state);
     }
 }
 
-impl<Marker> Copy for Id<Marker> {}
+impl<Marker> Copy for IdVecToken<Marker> {}
 
-impl<Marker> PartialEq for Id<Marker> {
+impl<Marker> PartialEq for IdVecToken<Marker> {
     fn eq(&self, other: &Self) -> bool {
-        self.element_id == other.element_id && self.container_id == other.container_id
+        self.container_id == other.container_id
     }
 }
 
-impl<Marker> Eq for Id<Marker> {}
+impl<Marker> Eq for IdVecToken<Marker> {}
 
-impl<Marker> PartialOrd for Id<Marker> {
+impl<Marker> PartialOrd for IdVecToken<Marker> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl<Marker> Ord for Id<Marker> {
+impl<Marker> Ord for IdVecToken<Marker> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.element_id.cmp(&other.element_id)
+        self.container_id.cmp(&other.container_id)
     }
 }
 
 #[derive(Clone, Debug)]
 pub struct IdVec<T, Marker = T> {
-    elements: BTreeMap<Id<Marker>, T>,
-    next_element_id: usize,
-    container_id: usize,
+    elements: Vec<T>,
+    token: IdVecToken<Marker>,
 }
 
 impl<T, Marker> IdVec<T, Marker> {
     pub fn new() -> IdVec<T, Marker> {
-        static VEC_ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
-
         IdVec {
-            elements: BTreeMap::new(),
-            next_element_id: 0,
-            container_id: VEC_ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::SeqCst),
+            elements: Vec::new(),
+            token: IdVecToken::new(),
         }
+    }
+
+    pub fn len(&self) -> usize {
+        self.elements.len()
+    }
+
+    pub fn token(&self) -> IdVecToken<Marker> {
+        self.token
     }
 
     fn next_id(&mut self) -> Id<Marker> {
         let id = Id {
-            element_id: self.next_element_id,
-            container_id: self.container_id,
-            marker: PhantomData,
+            element_id: self.elements.len(),
+            token: self.token(),
         };
-        self.next_element_id += 1;
         id
     }
 
     pub fn push(&mut self, val: T) -> Id<Marker> {
         let id = self.next_id();
-        self.elements.insert(id, val);
+        self.elements.push(val);
         id
     }
 
     pub fn iter(&self) -> impl Iterator<Item = (Id<Marker>, &T)> {
-        self.elements.iter().map(|(id, x)| (*id, x))
+        self.elements.iter().map(|x| {
+            (
+                Id {
+                    element_id: self.elements.len(),
+                    token: self.token(),
+                },
+                x,
+            )
+        })
     }
 
     pub fn get(&self, id: Id<Marker>) -> &T {
-        assert_eq!(id.container_id, self.container_id);
-        self.elements.get(&id).unwrap()
+        assert!(self.token().owns(id.clone()));
+        self.elements.get(id.inner()).unwrap()
     }
 
     pub fn get_mut(&mut self, id: Id<Marker>) -> &mut T {
-        assert_eq!(id.container_id, self.container_id);
-        self.elements.get_mut(&id).unwrap()
+        assert!(self.token().owns(id.clone()));
+        self.elements.get_mut(id.inner()).unwrap()
     }
 }
 
