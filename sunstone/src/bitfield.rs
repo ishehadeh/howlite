0850c4fbda6aabdac361238c3393e9a92bd5e6e5
Ian Shehadeh <ian@shehadeh.net>
2024-09-26 08:33:51 -0400

    bit field arithmetic

--- sunstone/src/bitfield.rs
+++ sunstone/src/bitfield.rs
@@ -1,9 +1,11 @@
+use num_prime::BitTest;
+
 use crate::{
     ops::{self, IntersectMut, UnionMut},
-    SetElement,
+    range::Range,
 };
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 /// A BitField stores set elements using a series of bits. A natural number `x` is in the set if the `x`th bit in this series is `1`.
 ///
 /// WIDTH is the number of bits in the set / 64 (e.g. WIDTH = 2 means 128 bit set)
@@ -11,9 +13,152 @@ pub struct BitField<const WIDTH: usize = 1> {
     field: [u64; WIDTH],
 }
 
+impl<const WIDTH: usize> std::fmt::Debug for BitField<WIDTH> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "BitField {{ ")?;
+        for (block_no, block) in self.field.iter().copied().enumerate() {
+            let mut rem_values = block;
+            for i in 0..u64::BITS as usize {
+                if rem_values & 1 != 0 {
+                    write!(f, " {},", i + block_no * 64)?
+                }
+
+                rem_values >>= 1;
+            }
+        }
+
+        write!(f, " }}")
+    }
+}
+
 impl<const WIDTH: usize> BitField<WIDTH> {
     pub fn new() -> Self {
         Default::default()
+    }
+
+    /// Returns the highest value in the set, or None if the set is empty
+    pub fn hi(&self) -> Option<usize> {
+        let mut offset = (WIDTH - 1) * u64::BITS as usize;
+        for block in self.field.iter().rev() {
+            let last_set_bit = u64::BITS - block.leading_zeros();
+            if last_set_bit > 0 {
+                return Some(offset + last_set_bit as usize - 1);
+            }
+
+            offset -= u64::BITS as usize;
+        }
+
+        None
+    }
+
+    /// Returns the lowest value in the set, or None if the set is empty
+    pub fn lo(&self) -> Option<usize> {
+        let mut offset = 0;
+        for block in self.field.iter() {
+            let first_set_bit = block.trailing_zeros() as u64;
+            if first_set_bit < u64::BITS as u64 {
+                return Some(offset + first_set_bit as usize);
+            }
+
+            offset += u64::BITS as usize;
+        }
+
+        None
+    }
+
+    /// Returns the lowest and highest value in the set, or None if the set is empty
+    pub fn range(&self) -> Option<Range<usize>> {
+        if let Some(lo) = self.lo() {
+            if let Some(hi) = self.hi() {
+                Some(Range::new(lo, hi))
+            } else {
+                unreachable!("hi() should only return None if lo() returned None!")
+            }
+        } else {
+            None
+        }
+    }
+
+    pub fn arith_add_scalar(mut self, n: usize) -> Self {
+        #[cfg(debug_assertions)]
+        {
+            if let Some(hi) = self.hi() {
+                assert!(n + hi < WIDTH * 64)
+            }
+        }
+        let value_shift = n % 64;
+        //bits that would be shifted off the end
+        let overflow_mask: u64 = !(u64::MAX >> value_shift);
+        let mut overflow_last = 0;
+        let mut overflow_now;
+        for x in &mut self.field {
+            // save bits that will overflow on this block
+            overflow_now = *x & overflow_mask;
+
+            *x <<= value_shift;
+
+            // apply overflow bits from prev iter
+            *x |= overflow_last;
+            overflow_last = overflow_now >> (u64::BITS as usize - value_shift);
+        }
+
+        let block_shift = n / 64;
+        if block_shift > 0 {
+            let mut i = self.field.len();
+            while i > 0 {
+                if i <= block_shift {
+                    self.field[i - 1] = 0;
+                } else {
+                    self.field[i - 1] = self.field[i - 1 - block_shift];
+                }
+
+                i -= 1;
+            }
+        }
+
+        self
+    }
+
+    pub fn arith_sub_scalar(mut self, n: usize) -> Self {
+        #[cfg(debug_assertions)]
+        {
+            if let Some(lo) = self.lo() {
+                assert!(lo > n)
+            }
+        }
+
+        let value_shift = n % 64;
+        //bits that would be shifted off the end
+        let overflow_mask: u64 = !(u64::MAX << value_shift);
+        let mut overflow_last = 0;
+        let mut overflow_now;
+        println!("{overflow_mask:064b}");
+        for x in self.field.iter_mut().rev() {
+            // save bits that will overflow on this block
+            overflow_now = *x & overflow_mask;
+            println!("*x  = {x:064b}");
+
+            *x >>= value_shift;
+            println!("*x' = {x:064b}");
+
+            // apply overflow bits from prev iter
+            *x |= overflow_last;
+            overflow_last = overflow_now << (u64::BITS as usize - value_shift);
+            println!("o   = {overflow_last:064b}");
+        }
+
+        let block_shift = n / 64;
+        if block_shift > 0 {
+            for i in 0..self.field.len() {
+                if i < self.field.len() - block_shift {
+                    self.field[i] = self.field[i + block_shift];
+                } else {
+                    self.field[i] = 0;
+                }
+            }
+        }
+
+        self
     }
 
     pub fn from_slice(slice: &[usize]) -> Self {
@@ -198,5 +343,49 @@ mod test {
             BitField::default().include(1).include(200).include(255),
             a.union(b)
         )
+    }
+
+    #[test]
+    pub fn add() {
+        let mut a: BitField<4> = BitField::default();
+        a.include_mut(10);
+        a.include_mut(1);
+        dbg!(&a);
+        assert!(a.includes(1));
+        assert!(a.includes(10));
+
+        let a = a.arith_add_scalar(100);
+        dbg!(&a);
+        assert!(a.includes(101));
+        assert!(a.includes(110));
+        assert!(!a.includes(10));
+
+        let a = a.arith_add_scalar(63);
+        dbg!(&a);
+        assert!(a.includes(164));
+        assert!(a.includes(173));
+        assert!(!a.includes(101));
+    }
+
+    #[test]
+    pub fn sub() {
+        let mut a: BitField<4> = BitField::default();
+        a.include_mut(200);
+        a.include_mut(150);
+        dbg!(&a);
+        assert!(a.includes(200));
+        assert!(a.includes(150));
+
+        let b = a.clone().arith_sub_scalar(43);
+        dbg!(&b);
+        assert!(b.includes(157));
+        assert!(b.includes(107));
+        assert!(!b.includes(200));
+
+        let a = a.arith_sub_scalar(100);
+        dbg!(&a);
+        assert!(a.includes(100));
+        assert!(a.includes(50));
+        assert!(!a.includes(150));
     }
 }
