427bc5a6c862fc08cb860f4b35de855321a3300b
Ian Shehadeh <ian@shehadeh.net>
2024-10-08 18:35:26 -0400

    manually impl traits so phantom data doesn't cause issues

--- howlite-syntax/src/tree/mod.rs
+++ howlite-syntax/src/tree/mod.rs
@@ -1,13 +1,48 @@
 mod read;
 mod write;
 
-use std::marker::PhantomData;
+use std::{fmt::Debug, hash::Hash, marker::PhantomData};
 
 pub use read::*;
 pub use write::*;
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+#[derive(Clone, Copy)]
 pub struct NodeId<T> {
     _t: PhantomData<T>,
     index: usize,
+}
+
+impl<T> Hash for NodeId<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.index.hash(state);
+    }
+}
+
+impl<T> std::fmt::Debug for NodeId<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "NodeId<{}>({:#08x})",
+            std::any::type_name::<T>(),
+            self.index,
+        )
+    }
+}
+
+impl<T> Eq for NodeId<T> {}
+impl<T> PartialEq for NodeId<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.index == other.index
+    }
+}
+
+impl<T> PartialOrd for NodeId<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl<T> Ord for NodeId<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.index.cmp(&other.index)
+    }
 }
