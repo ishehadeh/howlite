02dbf85d1beb65e880537595a0e7b4fc8cb18cf3
Ian Shehadeh <ian@shehadeh.net>
2024-11-05 19:51:46 -0500

    split out langctx

---
+++ howlite/src/langctx/mod.rs
@@ -0,0 +1,193 @@
+mod scope;
+
+use std::sync::{
+    atomic::{AtomicU64, Ordering},
+    RwLock,
+};
+
+use crate::{
+    errors::ErrorSet,
+    symtab::{Symbol, SyncSymbolTable},
+    CompilationError,
+};
+
+use dashmap::DashMap;
+pub use scope::*;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ScopeId(u64);
+
+pub struct LangCtx<SourceLocationT> {
+    pub symbols: SyncSymbolTable,
+    scopes: DashMap<ScopeId, Scope>,
+    pub errors: ErrorSet<SourceLocationT>,
+    scope_parent: RwLock<Vec<(ScopeId, ScopeId)>>,
+    pub root_scope_id: ScopeId,
+}
+
+/* #region impl LangCtx: ID Helpers */
+impl<L> LangCtx<L> {
+    /// create a new ScopeId, these identifiers are globally unique within the process
+    fn mint_scope_id() -> ScopeId {
+        static NEXT: AtomicU64 = AtomicU64::new(0);
+        ScopeId(NEXT.fetch_add(1, Ordering::Relaxed))
+    }
+}
+/* #endregion */
+
+/* #region impl LangCtx: General Functionality */
+impl<L> LangCtx<L> {
+    pub fn new() -> Self {
+        let scopes = DashMap::new();
+        let root_scope_id = Self::mint_scope_id();
+        scopes.insert(root_scope_id, Scope::default());
+
+        Self {
+            symbols: Default::default(),
+            scope_parent: Default::default(),
+            errors: Default::default(),
+            scopes,
+            root_scope_id,
+        }
+    }
+
+    /// Get the parent scope, return None if `scope` is the root scope.
+    ///
+    /// # Panics
+    /// This function panics if `scope` does not belong to this `LangCtx``
+    pub fn scope_parent(&self, child: ScopeId) -> Option<ScopeId> {
+        if child == self.root_scope_id {
+            None
+        } else {
+            let (child_from_vec, parent) = {
+                let scope_parent = self
+                    .scope_parent
+                    .read()
+                    .expect("LangCtx::scope_parent(): RwLock is poisoned!");
+                debug_assert!(
+                    scope_parent.is_sorted(),
+                    "LangCtx::scope_parent(): parent-child map isn't sorted, but we depend on binary search to find parents."
+                );
+                let pair_index = scope_parent
+                .binary_search_by_key(&child, |&(child, _parent)| child)
+                .expect("LangCtx::scope_parent(): could not find a parent for scope. There is either a bug in LangCtx's scope-create logic, or this scope does not belong to this instance of LangCtx");
+                scope_parent[pair_index]
+            };
+            debug_assert_eq!(child, child_from_vec, "LangCtx::scope_parent(): binary search found the wrong value, this should be impossible");
+            Some(parent)
+        }
+    }
+
+    pub fn scope_new(&self, parent: ScopeId) -> ScopeId {
+        let new_scope_id = Self::mint_scope_id();
+        {
+            let mut scope_parent = self
+                .scope_parent
+                .write()
+                .expect("LangCtx::scope_new(): RwLock is poisoned!");
+            debug_assert!(
+                scope_parent.is_sorted(),
+                "LangCtx::scope_new(): parent-child map isn't sorted, but we depend on binary search to find parents."
+            );
+            scope_parent.push((new_scope_id, parent));
+            debug_assert!(
+                scope_parent.is_sorted(),
+                "LangCtx::scope_new(): new scope IDs should always be increasing, but inserting at the end of the array did not maintain sort order"
+            );
+        }
+
+        debug_assert!(
+            self.scopes.insert(new_scope_id, Scope::default()).is_some(),
+            "LangCtx::scope_new(): scope exists, this should be impossible"
+        );
+
+        new_scope_id
+    }
+
+    /// Recursively search for a variable up the scope heirarchy
+    ///
+    /// # Panics
+    /// - Scope does not belong to this LangCtx
+    /// - Any panics associated with `self.scope_parent()`
+    pub fn var_get(&self, scope_id: ScopeId, var: Symbol) -> Option<VarDef> {
+        if let Some(scope) = self.scopes.get(&scope_id) {
+            if let Some(def) = scope.get_local(var) {
+                Some(def.clone())
+            } else if let Some(parent) = self.scope_parent(scope_id) {
+                self.var_get(parent, var)
+            } else {
+                // we're at the root scope
+                None
+            }
+        } else {
+            panic!(
+                "LangCtx::VarGet(): Scope is not associated with this LangCtx (scope={:?})",
+                scope_id
+            )
+        }
+    }
+
+    /// Define a variable in the given scope
+    ///
+    /// # Panics
+    /// - Scope does not belong to this LangCtx
+    pub fn var_def(&self, scope_id: ScopeId, var: Symbol, def: VarDef) {
+        if let Some(mut scope) = self.scopes.get_mut(&scope_id) {
+            scope.locals.push((var, def));
+        } else {
+            panic!(
+                "LangCtx::VarGet(): Scope is not associated with this LangCtx (scope={:?})",
+                scope_id
+            )
+        }
+    }
+
+    /// Recursively search for a type up the scope heirarchy
+    ///
+    /// # Panics
+    /// - Scope does not belong to this LangCtx
+    /// - Any panics associated with `self.scope_parent()`
+    pub fn ty_get(&self, scope_id: ScopeId, ty: Symbol) -> Option<TyDef> {
+        if let Some(scope) = self.scopes.get(&scope_id) {
+            if let Some(def) = scope.get_ty(ty) {
+                Some(def.clone())
+            } else if let Some(parent) = self.scope_parent(scope_id) {
+                self.ty_get(parent, ty)
+            } else {
+                // we're at the root scope
+                None
+            }
+        } else {
+            panic!(
+                "LangCtx::VarGet(): Scope is not associated with this LangCtx (scope={:?})",
+                scope_id
+            )
+        }
+    }
+
+    /// Define a named type in the given scope
+    ///
+    /// # Panics
+    /// - Scope does not belong to this LangCtx
+    pub fn ty_def(&self, scope_id: ScopeId, def: TyDef) {
+        if let Some(mut scope) = self.scopes.get_mut(&scope_id) {
+            scope.tys.push(def);
+        } else {
+            panic!(
+                "LangCtx::ty_def(): Scope is not associated with this LangCtx (scope={:?})",
+                scope_id
+            )
+        }
+    }
+
+    pub fn error(&self, err: CompilationError<L>) {
+        self.errors.insert(err);
+    }
+}
+/* #endregion */
+
+impl<L> Default for LangCtx<L> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
---
+++ howlite/src/langctx/scope.rs
@@ -0,0 +1,112 @@
+use std::rc::Rc;
+
+use howlite_typecheck::{Ty, TyBinder};
+use smallvec::SmallVec;
+
+use crate::{symtab::Symbol, CompilationError, CompilationErrorKind};
+
+use super::LangCtx;
+
+#[derive(Debug, Default)]
+pub struct Scope {
+    /// List of local variable definitions.
+    /// There can be duplicate symbols if a symbol is redefined
+    /// the list MUST be in the order variables are defined
+    pub locals: Vec<(Symbol, VarDef)>,
+
+    /// List of local variable definitions.
+    /// There can be duplicate symbols if a symbol is redefined
+    /// the list MUST be in the order variables are defined
+    pub tys: Vec<TyDef>,
+}
+
+#[derive(Debug, Clone)]
+pub struct VarDef {
+    pub assumed_ty: Rc<Ty<Symbol>>,
+    pub last_assignment: Rc<Ty<Symbol>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct TyDef {
+    pub name: Symbol,
+    pub params: Vec<(Symbol, Rc<Ty<Symbol>>)>,
+    pub ty: Rc<Ty<Symbol>>,
+}
+
+impl TyDef {
+    pub fn instantiate<SourceLocationT: Clone>(
+        &self,
+        err_location: SourceLocationT,
+        ctx: &LangCtx<SourceLocationT>,
+        params: &[Rc<Ty<Symbol>>],
+    ) -> Rc<Ty<Symbol>> {
+        if params.len() != self.params.len() {
+            ctx.error(CompilationError {
+                location: err_location.clone(),
+                kind: CompilationErrorKind::IncorrectTyParamCount {
+                    expected: self.params.len(),
+                    got: params.len(),
+                    ty: self.name,
+                },
+            });
+        };
+
+        let mut mapped_params: SmallVec<[(Symbol, Rc<Ty<Symbol>>); 4]> =
+            SmallVec::with_capacity(self.params.len());
+        for (i, given_ty) in params.iter().enumerate() {
+            if i >= self.params.len() {
+                break;
+            }
+            if let Err(e) = given_ty.is_assignable_to(&*self.params[i].1) {
+                ctx.error(CompilationError {
+                    location: err_location.clone(),
+                    kind: CompilationErrorKind::InvalidTyParam {
+                        param: self.params[i].0,
+                        source: e,
+                        ty: self.name,
+                    },
+                });
+                mapped_params.push((self.params[i].0, Rc::new(Ty::Hole)))
+            } else {
+                mapped_params.push((self.params[i].0, given_ty.clone()))
+            }
+        }
+
+        let mut bind = TyBinder::new(&mapped_params);
+        let bound = bind.bind(self.ty.clone());
+        for err in bind.errors() {
+            ctx.error(CompilationError {
+                location: err_location.clone(),
+                kind: err.clone().into(),
+            });
+        }
+
+        bound
+    }
+}
+
+impl Scope {
+    pub fn get_local(&self, name: Symbol) -> Option<&VarDef> {
+        self.locals
+            .iter()
+            .rev()
+            .find(|(l_name, _)| *l_name == name)
+            .map(|(_, var)| var)
+    }
+
+    pub fn get_local_mut(&mut self, name: Symbol) -> Option<&mut VarDef> {
+        self.locals
+            .iter_mut()
+            .rev()
+            .find(|(l_name, _)| *l_name == name)
+            .map(|(_, var)| var)
+    }
+
+    pub fn get_ty(&self, name: Symbol) -> Option<&TyDef> {
+        self.tys.iter().rev().find(|ty| ty.name == name)
+    }
+
+    pub fn get_ty_mut(&mut self, name: Symbol) -> Option<&mut TyDef> {
+        self.tys.iter_mut().rev().find(|ty| ty.name == name)
+    }
+}
