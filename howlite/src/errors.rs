51170daf6140b8b23047a1d88e9a5a8ce88d282f
Ian Shehadeh <ian@shehadeh.net>
2024-11-05 19:43:05 -0500

    add error struct

--- howlite/src/errors.rs
+++ howlite/src/errors.rs
@@ -3,6 +3,7 @@ use std::{
     sync::atomic::{AtomicU64, Ordering},
 };
 
+use dashmap::DashMap;
 use howlite_typecheck::{
     errors::{IncompatibleError, OperationError},
     BindError, Ty,
@@ -11,21 +12,78 @@ use thiserror::Error;
 
 use crate::symtab::Symbol;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct ErrorId(u64);
+/* #region ErrorSet */
+#[derive(Debug)]
+pub struct ErrorSet<SourceLocationT> {
+    pub errors: DashMap<ErrorId, CompilationError<SourceLocationT>>,
+}
 
-impl std::fmt::Display for ErrorId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "<Error:{:04}>", self.0)
+impl<SourceLocationT> Default for ErrorSet<SourceLocationT> {
+    fn default() -> Self {
+        Self {
+            errors: Default::default(),
+        }
     }
 }
 
-impl ErrorId {
+impl<SourceLocationT> ErrorSet<SourceLocationT> {
     pub fn mint_error_id() -> ErrorId {
         static NEXT: AtomicU64 = AtomicU64::new(0);
         ErrorId(NEXT.fetch_add(1, Ordering::Relaxed))
     }
+
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Define a variable in the given scope
+    pub fn insert(&self, err: CompilationError<SourceLocationT>) -> ErrorId {
+        let id = Self::mint_error_id();
+        debug_assert!(
+            self.errors.insert(id, err).is_none(),
+            "LangCtx::error(): error ({}) exists, this should be impossible",
+            id
+        );
+        id
+    }
+
+    pub fn iter(&self) -> ErrorIter<'_, SourceLocationT> {
+        ErrorIter {
+            inner: self.errors.iter(),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.errors.is_empty()
+    }
 }
+
+pub struct ErrorIter<'a, SourceLocationT> {
+    inner: dashmap::iter::Iter<'a, ErrorId, CompilationError<SourceLocationT>>,
+}
+
+pub struct ErrorEntry<'a, SourceLocationT> {
+    inner: dashmap::mapref::multiple::RefMulti<'a, ErrorId, CompilationError<SourceLocationT>>,
+}
+
+impl<'a, SourceLocationT> ErrorEntry<'a, SourceLocationT> {
+    pub fn id(&self) -> ErrorId {
+        self.inner.key().clone()
+    }
+
+    pub fn error(&self) -> &CompilationError<SourceLocationT> {
+        self.inner.value()
+    }
+}
+
+impl<'a, SourceLocationT> Iterator for ErrorIter<'a, SourceLocationT> {
+    type Item = ErrorEntry<'a, SourceLocationT>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().map(|inner| ErrorEntry { inner })
+    }
+}
+/* #endregion */
 
 #[derive(Clone, Debug)]
 pub struct CompilationError<SourceLocationT> {
@@ -60,4 +118,13 @@ pub enum CompilationErrorKind {
 
     #[error("expected integer bound to be a single Int, found: {got:?}")]
     InvalidIntegerBound { got: Rc<Ty<Symbol>> },
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ErrorId(u64);
+
+impl std::fmt::Display for ErrorId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "<Error:{:04}>", self.0)
+    }
 }
